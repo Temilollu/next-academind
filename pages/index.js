e9cea0e033f7f215ff4c7ceff73bf911930c59e4
temiloluwa odumosu <temiloluwaodumosu25@gmail.com>
2021-07-23 12:13:42 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <div>
+            i am home
+        </div>
+    )
+}
+
+export default HomePage
---
+++ pages/news.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewsPage = () => {
+    return (
+        <div>
+            i am news
+        </div>
+    )
+}
+
+export default NewsPage
